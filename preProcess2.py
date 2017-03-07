a390e3b63b592c873906da66fb78f0ef8f42a563
sjtuzza <zza39@qq.com>
2017-03-07 18:35:40 +0800

    Add files via upload

---
+++ preProcess2.py
@@ -0,0 +1,105 @@
+# -*- coding: UTF-8 -*-
+import sys
+from jieba import *
+import pymysql as mysql
+
+'''----------------------------------------------------------------------------
+Module name:
+preprocess.py
+
+Description:
+Tokenize the txt and calculate the tf-idf value
+Modified to operate with mysql
+Read from mysql and write back
+
+Author:
+Zhao Zian
+Rev. 2 10.26.2016
+----------------------------------------------------------------------------'''
+
+'''----------------------------------------------------------------------------
+Set default code
+Description: set default code as utf-8
+Author: ZRX
+----------------------------------------------------------------------------'''
+stdout = sys.stdout
+reload(sys)
+sys.stdout = stdout
+sys.setdefaultencoding('utf-8')
+
+
+'''----------------------------------------------------------------------------------
+fenci
+
+Purpose:
+Tokenize the txt and delete stop words
+
+Parameters:
+[in] string intext - the content of the law case
+[in] list stopWord - the list contain stopWords
+
+Return:
+string outText - the sliced content
+
+Author:
+Zhao Zian
+Rev. 2 10.26.2016
+----------------------------------------------------------------------------------'''
+
+'''----------------------------------------------------------------------------------
+fenci
+
+Revision: write file id at the beginning
+
+Author:
+Zhao Zian
+Rev. 2.1 2.22,2017
+----------------------------------------------------------------------------------'''
+
+
+def fenci(intext, stopword, file_id):
+    outtext = "%d " % file_id
+    # tokenize and delete the stop word
+    seg = set(lcut_for_search(intext)) - set(stopword)
+    for word in seg:
+        if (word != u'\n') and (word != u'\r') and (word != u'\t') and (word != ' '):
+            outtext += (str(word) + ' ')
+    return outtext
+
+
+''' Main section begins'''
+# the outFile
+outFile = open('/users/zw/desktop/texts.txt', 'w')
+# get the stopword list
+words = open('stopword_origin.txt', 'r')
+stopWord = words.read()
+
+# connect the database
+db = mysql.connect(user='root', passwd='12345678', db='lawcase', charset='utf8')
+cursor = db.cursor()
+cursor.execute("SET NAMES utf8")
+cursor.execute("FLUSH QUERY CACHE")
+
+# find the number of lawcases
+cursor.execute('select count(*) from divorce_data')
+tmp = cursor.fetchone()
+num = tmp[0]    # number of lawcases
+
+for i in range(num):
+    try:
+        # get the content of lawcase i+1
+        cursor.execute('select content from divorce_data where id = %d' % (i+1))
+        tmp = cursor.fetchone()
+        text = tmp[0]
+
+        outText = fenci(text, stopWord, i+1)
+        outFile.write(outText + '\n')
+    except Exception as e:
+       pass
+
+
+cursor.close()          # close the database
+db.close() 
+words.close()           # close the stopword file
+outFile.close()         # close the output file 
+
